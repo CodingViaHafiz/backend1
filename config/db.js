d033434665f1911b627601762db098cfd0181c1f
Hafiz Abdur rehman <hafiz1711205@gmail.com>
2025-08-06 17:02:39 +0500

    chnages in database

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 // require("dotenv").config();
 const connection = async () => {
   try {
-    await mongoose.connect(process.env.DB_URI);
+    await mongoose.connect(process.env.DATABASE_URL);
     console.log("conneted to database successfully!");
   } catch (error) {
     console.log("error while connecting to database: ", error);
